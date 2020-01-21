e8e1ccd50993d0fbf67fb0b8b4ac0f93cbc9ea01
JooChan <14jshin@gmail.com>
2020-01-21 02:13:28 -0500

    adding files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, () =>{
+	console.log('Server is running on port: ${port}');
+});
